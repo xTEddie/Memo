f5ec1bd3433ff6a4d83ba92a93be625d58ac1fec
Edward Tran <edward.tran@ge.com>
2016-05-17 10:56:40 -0400

    Add steps to install zprezto

---
+++ zprezto/install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install zsh
+cd
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+chsh -s /bin/zsh
+cp ~/.zprezto/runcoms/zshrc ~/
+mv ~/zshrc ~/.zshrc
+sudo reboot
